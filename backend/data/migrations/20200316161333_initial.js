95ee1f1a141f951dbb09debb638c3c324e7dcfe1
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-16 16:29:46 -0400

    basic file structure in place

---
+++ backend/data/migrations/20200316161333_initial.js
@@ -0,0 +1,12 @@
+
+exports.up = async function (knex) {
+  await knex.schema.createTable('users', tbl => {
+    tbl.increments('id')
+    tbl.string('username').notNullable().unique()
+    tbl.string('password').notNullable()
+  })
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('users')
+};
