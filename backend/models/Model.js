0ded81783b036a5d8984aec497cf245026b3570b
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-16 16:59:13 -0400

    added models

---
+++ backend/models/Model.js
@@ -0,0 +1,47 @@
+const db = require('../data/dbconfig')
+
+class Model {
+  constructor(tablename) {
+    this.tablename = tablename
+  }
+
+  find() {
+    return db(this.tablename)
+  }
+
+  findBy(filter) {
+    return db(this.tablename)
+      .where(filter)
+      .first()
+  }
+
+  findById(id) {
+    return db(this.tablename)
+      .where({ id })
+      .first()
+  }
+
+  add(item) {
+    return db(this.tablename)
+      .insert(item)
+      .returning('*')
+  }
+
+  update(id, item) {
+    return db(this.tablename)
+      .where({ id })
+      .update(item)
+      .returning('*')
+  }
+
+  remove(id) {
+    return db(this.tablename)
+      .where({ id })
+      .del()
+  }
+
+}
+
+export const User = new Model('users')
+
+export default Model
